9f349574cf903267aa1850e82f40377b08413421
yu_sato <yu_sato@net-nsi.co.jp>
2024-04-23 06:41:46 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-//↓元々あったがカリキュラムにはないのでコメント化
-//use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsersController; // 追記
+
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\MicropostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,28 +11,18 @@ use App\Http\Controllers\UsersController; // 追記
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [MicropostsController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-//↓元々あったがカリキュラムにはないのでコメント化
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//});
-
-Route::group(['middleware' => ['auth']], function () {                                    // 追記
-    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
-});  
+Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
+    Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
+});
